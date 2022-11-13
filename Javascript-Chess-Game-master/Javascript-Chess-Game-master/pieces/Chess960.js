2a604da1f7d0969ee46ca028f51c7868e2afbe0c
sbrockmeyer <79857788+sbrockmeyer@users.noreply.github.com>
2022-11-13 11:56:32 -0700

    blarg

--- Javascript-Chess-Game-master/Javascript-Chess-Game-master/pieces/Chess960.js
+++ Javascript-Chess-Game-master/Javascript-Chess-Game-master/pieces/Chess960.js
@@ -1,7 +1,30 @@
 // randomly place rook
 
-let black1 = document.getElementById('black1');
+const black1 = document.querySelector(".black1");
+const id1 = document.getElementsByName("img").id;
+let randnum = Math.floor(Math.random()*2);
 
-let randnum = Math.floor(Math.random()*9);
 
-console.log(randnum);
+
+const placingRandomRook = () =>{
+    console.log(randnum);
+    switch (randnum) {
+        case 0:
+            black1.src = "img/blackKnight.png";
+            console.log(`i changed to kight`);
+            // id1 = document.getElementById('black1').id1 = 'blackRook1';
+            break;
+        case 1:
+            console.log(`i changed to something in 1`)
+            break;
+        default:
+            break;
+    }
+}
+
+black1.addEventListener("load", () =>{
+    // console.log(randnum);
+    placingRandomRook();
+});
+
+// console.log(randnum);
